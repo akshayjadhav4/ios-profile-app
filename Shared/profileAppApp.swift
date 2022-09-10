7b3fe8529264b0334b19bbf84861fe9a312718d0
Akshay Jadhav <akshayjadhav461999@gmail.com>
2022-09-10 18:20:38 +0530

    Initial Commit

---
+++ Shared/profileAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  profileAppApp.swift
+//  Shared
+//
+//  Created by Akshay Anil Jadhav on 10/09/22.
+//
+
+import SwiftUI
+
+@main
+struct profileAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
